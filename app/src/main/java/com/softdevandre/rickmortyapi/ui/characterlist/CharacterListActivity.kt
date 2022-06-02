fb0cd638f144e9826e71d5e6cd5dd4936829d288
softdevandre <softdev.andre@gmail.com>
2022-06-02 20:12:15 -0300

    setted up character list RecyclerView

--- app/src/main/java/com/softdevandre/rickmortyapi/ui/characterlist/CharacterListActivity.kt
+++ app/src/main/java/com/softdevandre/rickmortyapi/ui/characterlist/CharacterListActivity.kt
@@ -1,6 +1,13 @@
 package com.softdevandre.rickmortyapi.ui.characterlist
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class CharacterListActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 }
