e4650cf8738003d7f4563170399d0d5eb38ce568
softdevandre <softdev.andre@gmail.com>
2022-06-05 20:25:06 -0300

    setted up character list Adapter

--- app/src/main/java/com/softdevandre/rickmortyapi/ui/characterlist/CharacterListActivity.kt
+++ app/src/main/java/com/softdevandre/rickmortyapi/ui/characterlist/CharacterListActivity.kt
@@ -2,12 +2,34 @@ package com.softdevandre.rickmortyapi.ui.characterlist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.softdevandre.rickmortyapi.databinding.ActivityCharacterListBinding
 
 class CharacterListActivity : AppCompatActivity(){
 
+    private val binding by lazy {
+        ActivityCharacterListBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.rvCharacterList.layoutManager = LinearLayoutManager(this)
+        binding.rvCharacterList.adapter = CharacterListAdapter(charMockList())
 
     }
+
+    private fun charMockList(): List<Character> {
+        return arrayListOf(
+            Character("André", "Alive"),
+            Character("José", "Alive"),
+            Character("Pelé", "Alive"),
+            Character("Mané", "Alive"),
+        ).toList()
+    }
+
+    data class Character(val charName: String, val charStatus: String)
+
 }
+
--- app/src/main/java/com/softdevandre/rickmortyapi/ui/characterlist/CharacterListAdapter.kt
+++ app/src/main/java/com/softdevandre/rickmortyapi/ui/characterlist/CharacterListAdapter.kt
@@ -1,4 +1,31 @@
 package com.softdevandre.rickmortyapi.ui.characterlist
 
-class CharacterListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.softdevandre.rickmortyapi.databinding.CharacterListItemBinding
+
+class CharacterListAdapter(private val characters: List<CharacterListActivity.Character>) :
+    RecyclerView.Adapter<CharacterListAdapter.ViewHolder>() {
+
+    class ViewHolder(private val binding: CharacterListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: CharacterListActivity.Character) = with(binding) {
+            tvCharName.text = item.charName
+            tvCharStatus.text = item.charStatus
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            CharacterListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(characters[position])
+    }
+
+    override fun getItemCount() = characters.size
 }
